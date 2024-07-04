7aa5ef04a7c825a339395c41631d69c78e820006
SWETA_RAJ <swetaraj@google.com>
2024-07-04 17:18:34 +0530

    adding modules

---
+++ variables.tf
@@ -0,0 +1,27 @@
+variable "tfe_org" {
+  description = "The name of the organization associated with the registry module"
+  type        = string
+  default     = null
+  sensitive   = true
+}
+
+variable "gh_oauth_token" {
+  type      = string
+  sensitive = true
+}
+
+variable "tfe_teams_api_token" {
+  type      = string
+  sensitive = true
+}
+
+variable "modules_registry" {
+  type = map(object({
+    repo_identifier         = string
+    repo_branch             = optional(string, "")
+    ghe_installation_id     = optional(string, null)
+    tags                    = bool
+  }))
+}
+
+
